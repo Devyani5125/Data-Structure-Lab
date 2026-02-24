6a21924fa499813409510e0923231f02884bb326
Devyani Patil <patildevyani5125@gmail.com>
2026-02-24 16:33:52 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,37 @@
+// Experiment 4:
+// Sorting Algorithms - Bubble Sort       
+// Bubble Sort is a simple sorting algorithm that repeatedly steps through the list, 
+// compares adjacent elements and swaps them if they are in the wrong order.
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
